26b693322e99cbf97c2f9616e99d389b2a9033ee
Lagrieta <lagriet@hotmail.com>
2022-03-31 17:33:03 -0600

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "PRELOAD";
+const accessCode2 = "MATTER";
+const accessCode3 = "RESTITUTION";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("Clave 1", 100,50)
+    fill("lightblue")
+    text("Cómo se le llama a la función de carga de imágenes", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("Clave 2", 700,150)
+    fill("lightblue")
+    text("¿Qué librería nos ayuda a dar materia?", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("Clave 3", 100,250)
+    fill("lightblue")
+text("¿Cómo se llama al efecto de gravedad", 100,270)
+
+}
